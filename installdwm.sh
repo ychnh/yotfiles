bbc83b9a9a7a9e81a1a0b29ea896f00ba87e08b0
ychnh <36415513+ychnh@users.noreply.github.com>
2023-05-26 21:01:34 +0900

    Create installdwm.sh

---
+++ installdwm.sh
@@ -0,0 +1,14 @@
+sudo apt-get install build-essential libx11-dev libxinerama-dev sharutils suckless-tools
+
+cd /usr/local/src
+sudo wget http://dl.suckless.org/dwm/dwm-6.0.tar.gz
+sudo tar xvzf dwm-6.0.tar.gz
+chown -R `id -u`:`id -g` dwm-6.0
+cd dwm-6.0
+
+sudo make clean install
+
+sudo apt-get install dwm
+sudo cp /usr/share/xsessions/dwm.desktop{,.bak}
+sudo apt-get purge dwm
+sudo mv /usr/share/xsessions/dwm.desktop{.bak,}
