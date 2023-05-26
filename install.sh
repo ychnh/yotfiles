d29296c9026fc1237ef201196abd6d076ad4a3ba
ychnh <36415513+ychnh@users.noreply.github.com>
2023-05-26 22:20:35 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ install needed packages
 sudo apt-get install emacs
 sudo apt-get install tmux
 sudo apt-get install nvim
-git clone https://github.com/syl20bnr/spacemacs
+git clone https://github.com/syl20bnr/spacemacs $HOME/.emacs.d
 install dwm
