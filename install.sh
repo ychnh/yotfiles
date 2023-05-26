b371111dbca8a8451a0594157847da3b9f84dc62
ychnh <36415513+ychnh@users.noreply.github.com>
2023-05-27 00:14:44 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,8 @@ sudo apt-get install emacs
 sudo apt-get install tmux
 sudo apt-get install nvim
 git clone https://github.com/syl20bnr/spacemacs $HOME/.emacs.d
+sudo apt-get install texlive-latex-base
+sudo apt-get install dvipng
+
+
 install dwm
