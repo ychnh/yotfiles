4459275e8ae8d1d5daaa669dfbc51584cb80be54
ychnh <36415513+ychnh@users.noreply.github.com>
2023-05-27 00:19:10 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ sudo apt-get install nvim
 git clone https://github.com/syl20bnr/spacemacs $HOME/.emacs.d
 sudo apt-get install texlive-latex-base
 sudo apt-get install dvipng
-
+sudo apt install texlive-latex-extra
+sudo ln -s /usr/bin/dvipng /usr/local/bin
+sudo ln -s /usr/bin/latex /usr/local/bin
 
 install dwm
