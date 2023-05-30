e242f8a1cbfc0d797360b4b2e92ee224d9d14b4f
ychnh <36415513+ychnh@users.noreply.github.com>
2023-05-30 16:14:05 +0900

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,10 +1,43 @@
+
 (defun my-custom-function ()
   "My custom function that prints a message."
   (interactive)
   (message "Hello, Emacs!"))
-(global-set-key (kbd "C-c C-f") 'my-custom-function)
 
 ;; =========
 (setq org-format-latex-options
       '(:foreground "white" :background "#2e2e2e" :scale 1.5 :html-foreground "navy" :html-background "lemonchiffon"))
 (global-linum-mode t)
+;;(setq org-adapt-indentation nil)
+;;(setq org-indent-mode -1)
+
+;;==============================================
+(setq org-startup-folded 'showeverything)
+(setq org-html-postamble nil)
+
+(defun my-org-html-head (exporter)
+  (when (eq exporter 'html)
+    (setq-local org-html-head-extra
+                (concat "<link rel=\"stylesheet\" type=\"text/css\" href=\"styles.css\" />\n"
+                        org-html-head-extra))))
+
+(add-hook 'org-export-before-processing-hook 'my-org-html-head)
+
+
+
+;; ==============================================
+
+(defun org-latex-preview-all-sections ()
+  "Run `org-latex-preview' for all sections in the buffer."
+  (interactive)
+  (let ((org-heading-regexp "^\\*+ "))
+    (save-excursion
+      (goto-char (point-min))
+      (while (re-search-forward org-heading-regexp nil t)
+        (org-latex-preview)))))
+
+(add-hook 'org-mode-hook 'org-latex-preview-all-sections)
+
+(global-set-key (kbd "C-l") 'org-latex-preview)
+(global-set-key (kbd "C-s") 'save-buffer)
+(global-set-key (kbd "C-l C-a") 'org-latex-preview-all-sections)
