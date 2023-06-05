498b1e1769463528b921dce027051d87ddc38888
ychnh <36415513+ychnh@users.noreply.github.com>
2023-06-05 20:37:46 +0900

    Update installdwm.sh

--- installdwm.sh
+++ installdwm.sh
@@ -1,8 +1,9 @@
 sudo apt-get install build-essential libx11-dev libxinerama-dev sharutils suckless-tools
 
 cd /usr/local/src
-sudo wget http://dl.suckless.org/dwm/dwm-6.2.tar.gz
-sudo tar xvzf dwm-6.2.tar.gz
+#sudo wget http://dl.suckless.org/dwm/dwm-6.2.tar.gz
+#sudo tar xvzf dwm-6.2.tar.gz
+git clone https://github.com/ychnh/dwm6.2-custom dwm-6.2
 chown -R `id -u`:`id -g` dwm-6.2
 cd dwm-6.2
 
